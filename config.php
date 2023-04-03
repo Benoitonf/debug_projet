6a660416903d1573471a494f34b66d6327e091b6
Machral <floflo60-70@hotmail.fr>
2023-04-03 16:44:48 +0200

    Information pour la connection à la BDD dans un fichier .env

--- config.php
+++ config.php
@@ -6,10 +6,12 @@
  */
 
 // Pourquoi pas utiliser un .ENV ?
+$env_vars = parse_ini_file(__DIR__ . '/.env');
+
 return array(
-    'host' => '127.0.0.1',
-    'user' => 'root',
-    'password' => 'root',
-    'port'=> 8889,
-    'name' => 'phptoolbox'
+    'host' => $env_vars['DB_HOST'],
+    'user' => $env_vars['DB_USER'],
+    'password' => $env_vars['DB_PASSWORD'],
+    'port'=> $env_vars['DB_PORT'],
+    'name' => $env_vars['DB_NAME']
 );
