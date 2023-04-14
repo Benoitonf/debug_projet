2c27394f652ca15b43e1f8a5f9fdc29b9ad8dbfb
Machral <floflo60-70@hotmail.fr>
2023-04-14 09:49:02 +0200

    Suppression des librairies inutilisées

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,4 @@
     <script src="<?= home_url(); ?>/assets/js/main.js"></script>
-    <script defer src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    
     <script src="<?= home_url(); ?>/assets/js/bootstrap.js"></script>
 
     </body>
